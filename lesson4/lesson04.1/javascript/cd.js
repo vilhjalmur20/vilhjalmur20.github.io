2ae573b3bab6cf86a21117dab5a44e7853eb61a2
Vilhjalmur Olafsson <villi@Vilhjalmurs-MacBook-Air.local>
2020-01-30 19:48:26 -0700

    commit

--- lesson4/lesson04.1/javascript/cd.js
+++ lesson4/lesson04.1/javascript/cd.js
@@ -1,3 +1,3 @@
-const options={day: 'long', day: 'numeric', month: 'name', year: 'numeric'};
+const options={day: 'long', days: 'numeric', month: 'name', year: 'numeric'};
 
 document.getElementById('cd').textContent=new Date().toLocaleDateString('en-US', options);
