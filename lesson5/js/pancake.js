5388adf61191fb2da39078331d57f93ef1b35c14
Vilhjalmur Olafsson <villi@Vilhjalmurs-MacBook-Air.local>
2020-02-06 20:28:54 -0700

    commit

---
+++ lesson5/js/pancake.js
@@ -0,0 +1,6 @@
+function pancake() {
+    var date = new Date();
+    console.log(date.getDay());
+    if(date.getDay() === 5) {document.getElementById("friday").innerHTML = "Saturday = Preston Pancakes in the Park! <br>9:00 a.m.Saturday at the City Park Pavilion.";
+    } 
+}
