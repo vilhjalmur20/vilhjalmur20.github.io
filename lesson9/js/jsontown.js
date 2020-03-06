04526fb8a893e368adca00c59f34d6076e30e125
Vilhjalmur Olafsson <villi@Vilhjalmurs-MacBook-Air.local>
2020-03-05 19:46:28 -0700

    commit

--- lesson9/js/jsontown.js
+++ lesson9/js/jsontown.js
@@ -8,6 +8,7 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         console.table(jsonObject);
+        const towns = jsonObject['towns'];
 
             //selecting towns out of Json
         for (let i = 0; i < towns.length; i++) {
@@ -33,7 +34,7 @@ fetch(requestURL)
                 // putting json elements into order to display in HTML 
             h2.textContent = towns.name;
             h4.textContent = towns.motto;
-            year.textContent = "Year Founded: " + towns.yearFounded;
+            towns.yearFounded = towns[i].yearFounded;
             annual_rainfall.textContent = "Annual Rainfall: " + towns.averageRainfall;
             population.textContent = "Town Population: " + towns.currentPopulation;
             image.setAttribute("src", towns[i].photo); 
