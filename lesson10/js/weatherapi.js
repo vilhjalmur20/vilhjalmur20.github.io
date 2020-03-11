e29a6af90f5a38797f2d5907fd2cbe914fcee017
Vilhjalmur Olafsson <villi@Vilhjalmurs-MacBook-Air.local>
2020-03-11 14:48:14 -0600

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,15 +1,17 @@
-const apiURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8f7bd10376e2fe83de7f28f324da5709";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8f7bd10376e2fe83de7f28f324da5709";
 
+fetch(apiURL)
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
+    
     console.log(jsObject);
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+    const imagesrc = "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; 
     const desc = jsObject.weather[0].description;  
 
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    document.getElementById('imagesrc').textContent = imagesrc;
-    document.getElementById('icon').setAttribute('src', imagesrc);
-    document.getElementById('icon').setAttribute('alt', desc);  
+    document.getElementById("current-temp").textContent = jsObject.main.temp;
+    document.getElementById("imagesrc").textContent = imagesrc;
+    document.getElementById("icon").setAttribute("src", imagesrc);
+    document.getElementById("icon").setAttribute("alt", desc);  
   });
