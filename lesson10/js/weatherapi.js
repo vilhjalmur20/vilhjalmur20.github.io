966227a9fbd013a5397b561fb6f0abb6d9ac7f7b
Vilhjalmur Olafsson <villi@Vilhjalmurs-MacBook-Air.local>
2020-03-11 14:18:00 -0600

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,43 +1,15 @@
-/*const requestURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8f7bd10376e2fe83de7f28f324da5709";*/
+const apiURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8f7bd10376e2fe83de7f28f324da5709";
 
-
-const apiURL = ""api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8f7bd10376e2fe83de7f28f324da5709";";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+    const desc = jsObject.weather[0].description;  
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);  
   });
-
-  /*
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.table(jsonObject);  // temporary checking for valid response and data parsing
-
-        const prophets = jsonObject['prophets'];
-
-        for (let i = 0; i < prophets.length; i++) {
-            let prophets_display = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let birth = document.createElement('p');
-            let place = document.createElement('p');
-            let image = document.createElement('img');
-
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            birth.textContent = "Date of Birth: " + prophets[i].birthdate;
-            place.textContent = "Place of Birth: " + prophets[i].birthplace;
-
-
-            image.setAttribute('src', prophets[i].imageurl); 
-            image.setAttribute('alt', "picture of: " + h2.textContent);
-
-            prophets_display.appendChild(h2);
-            prophets_display.appendChild(birth);
-            prophets_display.appendChild(place);
-            prophets_display.appendChild(image);
-
-            document.querySelector('div.cards').appendChild(prophets_display);
-        }
-    });
