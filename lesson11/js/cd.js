68348f867f45460fe8f0927f3aace6466c0f2a79
Vilhjalmur Olafsson <villi@Vilhjalmurs-MacBook-Air.local>
2020-03-17 15:07:31 -0600

    commit

--- lesson11/js/cd.js
+++ lesson11/js/cd.js
@@ -1,10 +1,10 @@
-const dayz = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const monthz = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const dayy = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const monthy = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 let today = new Date();
 let day = today.getDate();
-let dayName = dayz[today.getDay()];
-let month = monthz[today.getMonth()];
+let dayName = dayy[today.getDay()];
+let month = monthy[today.getMonth()];
 let year = today.getFullYear();
 
 document.getElementById("cd").textContent = `${dayName}, ${day} ${month} ${year}`
